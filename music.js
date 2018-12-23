75d19f9d923ba28ed714975f9d6dd283aa8df044
ColorizeMySky <colorizemysky@gmail.com>
2018-12-24 01:01:39 +0300

    Create music search

--- music.js
+++ music.js
@@ -1,4 +1,5 @@
 const musicInfo = [];
+let playList = [];
 
 function addSongFromField(event) {
   event.preventDefault();
@@ -23,14 +24,60 @@ function renderList() {
   $list.empty();
 
   for (const info of musicInfo) {
-    const $item = $('<li class="list-group-item">').text(info);
-
-    $list.append($item)
+    if(info) {
+      const $item = $('<li class="list-group-item">').text(info);
+      $list.append($item);
+    }
   }
 }
 
-$('#getPlaylistBtn').click(function (event) {
-  // TODO: Display a list of music.
-  // You may use anything from musicInfo.
-  console.log('Testing Music Call');
+$('#getPlaylistBtn').click(function(event) {
+
+  //let requestUrls = musicInfo.map(req => 'https://itunes.apple.com/search?term=' + req + '&limit=30');
+  
+  $.when(
+    $.each(musicInfo, function(index, value) {    
+      $.ajax({
+        url: 'https://itunes.apple.com/search?term=' + value + '&limit=30',
+        dataType: 'json',
+        success: function(data) {
+          $.each(data.results, function(index, value){
+            playList.push(value);
+          })
+        }
+      })
+    })
+  ).then(function() {
+    playList.sort((a, b) => Math.random() - 0.5);    
+  }).then(function() {
+    if(playList.length == 0) {
+      $(".play").append("<h1>No matches. Try again.</h1>");
+      return;
+    }
+    playList = playList.slice(0, 30);
+    console.log(playList)
+  }).then(function(){
+    $.each(playList, function(index, value){
+      $(".play").append("<div class='row'><div class='col-md-2'><img src='" + value.artworkUrl100 + "' alt='" + value.trackName + "' /></div><div class='col-md-6'><h3>" 
+        + value.trackName + "</h3><h4>"
+        + value.artistName + "</h4><p>"
+        + value.collectionName + "</p></div><div class='col-md-2 playSound btn'>Play<audio src='" + value.previewUrl + "'></audio></div></div>");
+    })
+  }).then(function(){
+    $('.playSound').click(function(event) {
+      console.log(event.target.querySelector('audio'));
+      $(this).find('audio').trigger("play");
+    })
+  })
+
 });
+
+
+/*
+  value.artistName,
+  value.trackName,
+  value.collectionName, //album
+  value.previewUrl, //track
+  value.artworkUrl100, //img
+  value.primaryGenreName //
+*/
